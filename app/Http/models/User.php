3c8d75ab6eca0a0ad4c700e6a1dc0e6d002bd481
kimJaeHyeon <kmr2644@gmail.com>
2023-11-15 21:38:09 +0900

    feat: 유저조회메서드 구현

---
+++ app/Http/models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\models;
+
+use App\Http\daos\UserDao;
+
+class User
+{
+    private int $id;
+    private string $nickName;
+    private string $password;
+
+    /**
+     * @param int $id
+     * @param string $nickName
+     * @param string $password
+     */
+    public function __construct(int $id, string $nickName, string $password)
+    {
+        $this->id = $id;
+        $this->nickName = $nickName;
+        $this->password = $password;
+    }
+
+
+    public function isMatchedPassword(string $password): bool
+    {
+        return $password === $this->password;
+    }
+
+    public static function createWithModel($model): User
+    {
+        return new User($model->id, $model->nick_name, $model->password);
+    }
+}
